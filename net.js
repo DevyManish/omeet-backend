bf90fe2935c003bd91dd52f4fa26d13b60348cae
DevyManish <121728309+DevyManish@users.noreply.github.com>
2025-04-24 00:07:35 +0530

    init web socket express app

---
+++ net.js
@@ -0,0 +1,15 @@
+const os = require('os');
+
+const getLocalIpAddress = () => {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '0.0.0.0'; // Fallback
+}
+
+module.exports = getLocalIpAddress;
